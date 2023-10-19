2fd67f8920bee71cee53a5bbfef07a331b68c4bc
bakasibruceto <abla09027@gmail.com>
2023-10-19 12:52:02 +0800

    email logo added

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,14 @@
+
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+{{-- <a href="{{ $url }}" style="display: inline-block;"> --}}
+@if (trim($slot) === 'CCS FORUMS')
+<img src="https://raw.githubusercontent.com/bakasibruceto/CCS-FORUMS/main/public/logo.png" class="logo" alt="Laravel Logo">
 @else
 {{ $slot }}
+
 @endif
-</a>
+{{-- </a> --}}
 </td>
 </tr>
