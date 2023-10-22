341c98a0fdd45e0ece460b47a2e22729786a160e
bakasibruceto <abla09027@gmail.com>
2023-10-22 20:47:17 +0800

    changed View to User

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-class ViewController extends Controller
+class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
