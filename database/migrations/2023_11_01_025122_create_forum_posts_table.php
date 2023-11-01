bfedf4142f761990cf70030142bdd0cba401afdd
bakasibruceto <abla09027@gmail.com>
2023-11-01 15:39:32 +0800

    Create 2023_11_01_025122_create_forum_posts_table.php

---
+++ database/migrations/2023_11_01_025122_create_forum_posts_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('forum_posts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('forum_posts');
+    }
+};
