bd0b5839c2fa80cf690b3d2567d710a0d59351b9
bakasibruceto <abla09027@gmail.com>
2023-11-05 22:55:42 +0800

    frontend ui

---
+++ resources/views/components/like-comment-share.blade.php
@@ -0,0 +1,19 @@
+<div class="flex justify-between items-center px-11 pb-3">
+    <button class="flex items-center">
+        <ion-icon class="h-4 w-4 text-red-500 items-center" name="heart-outline"></ion-icon>
+        <span class="text-gray-600 text-sm pl-2">Like</span>
+    </button>
+    <button class="flex items-center show-modal">
+        <ion-icon class="h-4 w-4 text-blue-500 items-center" name="chatbubbles-outline"></ion-icon>
+         <span class="text-gray-600 text-sm pl-2">Comment</span>
+    </button>
+    <button class="flex items-center">
+        <ion-icon class="h-4 w-4 text-blue-500 items-center" name="share-social-outline"></ion-icon>
+        <span class="text-gray-600 text-sm pl-2">Share</span>
+    </button>
+</div>
+<div class="flex w-full border-t border-gray-100"></div>
+                <div class="mt-2 mx-5 flex flex-row justify-start text-xs">
+                    <div class="flex text-gray-700 font-normal rounded-md mb-2 mr-4 items-center">Likes: <div class="ml-1 text-gray-400 text-ms"> 30</div></div>
+                    <div class="flex text-gray-700 font-normal rounded-md mb-2 mr-4 items-center">Comments:<div class="ml-1 text-gray-400 text-ms"> 10</div></div>
+                </div>
