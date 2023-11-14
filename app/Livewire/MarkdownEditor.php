480c9da82ef9e2e9be9e4b3786ed8ab3b1054c72
bakasibruceto <abla09027@gmail.com>
2023-11-15 01:57:24 +0800

    Update MarkdownEditor.php

--- app/Livewire/MarkdownEditor.php
+++ app/Livewire/MarkdownEditor.php
@@ -3,10 +3,15 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\ForumPost;
+use Illuminate\Support\Facades\Auth;
 
 class MarkdownEditor extends Component
 {
+    protected $layout = 'layouts.app';
     public $markdown = '';
+    public $subject = '';
+    public $tags = '';
     public $previewMode = false;
 
     public function render()
@@ -25,4 +30,49 @@ class MarkdownEditor extends Component
     {
         $this->previewMode = !$this->previewMode;
     }
+
+    public function savePost()
+    {
+        try {
+            // Validate the input if needed
+            $this->validate([
+                'subject' => 'required|max:255',
+                'tags' => 'required|max:255',
+                'markdown' => 'required',
+            ]);
+
+            // Debugging: Dump the validated data
+            dump([
+                'user_id' => Auth::user()->id,
+                'subject' => $this->subject,
+                'tags' => $this->tags,
+                'content' => $this->markdown,
+            ]);
+
+            // Save the post to the database
+            ForumPost::create([
+                'user_id' => Auth::user()->id,
+                'subject' => $this->subject,
+                'tags' => $this->tags,
+                'content' => $this->markdown,
+            ]);
+
+            // Reset the form fields after saving
+            $this->subject = '';
+            $this->tags = '';
+            $this->markdown = '';
+
+            // Optionally, you can redirect the user after saving
+            session()->flash('success', 'Post saved successfully!');
+            return redirect()->route('/'); // Replace with the actual route name
+
+        } catch (\Exception $e) {
+            // Log the exception for debugging
+            \Log::error($e);
+
+            session()->flash('error', 'An error occurred while saving the post.');
+            return redirect()->back();
+        }
+    }
+
 }
