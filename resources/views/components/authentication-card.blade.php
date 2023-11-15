a2307873e2bff52e761d036a1a5ddb1f782ede99
kaiii07 <131762936+kaiii07@users.noreply.github.com>
2023-11-15 18:03:34 +0800

    up

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,9 @@
 <div class="w-full xl:w-3/4 lg:w-11/12 flex sm:justify-center items-center pt-6 sm:pt-0 bg-white">
-    <div class="w-full sm:max-w-md sm:py-4 mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg md:ml-14">
+    <div class="w-full sm:max-w-md sm:py-4 mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg md:-mr-32">
         {{ $slot }} 
        
     </div>
-    <div class="order-first sm:order-first h-screen flex justify-center items-center md:ml-10">
+    <div class="order-first sm:order-first h-screen flex justify-center items-center md:ml-44 md:mr-5">
        
         {{ $logo }}
     </div>
