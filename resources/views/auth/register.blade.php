9ede1b0b43e9651a253c8038b8d2dd8016bbbf49
kaiii07 <131762936+kaiii07@users.noreply.github.com>
2023-11-15 17:33:08 +0800

    up

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,37 +1,32 @@
 <x-guest-layout>
     <x-authentication-card>
         <x-slot name="logo">
-            <x-authentication-card-logo />
+            <x-authentication-card-register />
         </x-slot>
 
         <x-validation-errors class="mb-4" />
-
+        <h1 class="text-gray-800 font-bold text-center text-3xl mb-6">Welcome, Clever Fox!</h1>
         <form method="POST" action="{{ route('register') }}">
             @csrf
 
             <div>
-                <x-label for="name" value="{{ __('Name') }}" />
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
+                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" placeholder="Name" />
             </div>
 
-            <div class="mt-4">
-                <x-label for="username" value="{{ __('Username') }}" />
-                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('name')" required autofocus autocomplete="username" />
+            <div class="mt-3">
+                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('name')" required autofocus autocomplete="username" placeholder="Username"/>
             </div>
 
-            <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" />
+            <div class="mt-3">
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="email" placeholder="Email"/>
             </div>
 
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            <div class="mt-3">
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" placeholder="Password"/>
             </div>
 
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
+            <div class="mt-3">
+                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" placeholder="New Password"/>
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
--- resources/views/components/authentication-card-register.blade.php
+++ resources/views/components/authentication-card-register.blade.php
@@ -5,7 +5,7 @@
         <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
         
     </svg> --}}
-    <img class="max-w-md h-auto mx-auto my-auto" src="../login.jpg" alt="CCS LOGO">
+    <img class="max-w-md h-auto mx-auto my-auto" src="../register.png" alt="CCS LOGO">
 
 
 
