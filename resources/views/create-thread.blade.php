df4c0b59cdfca72bf464fcae73d7fc39f9a18f16
bakasibruceto <abla09027@gmail.com>
2023-11-15 15:35:00 +0800

    create thread page

--- resources/views/create-thread.blade.php
+++ resources/views/create-thread.blade.php
@@ -4,27 +4,8 @@
 
         Please search for your question before posting your thread by using the search box in the navigation bar.
         Want to share large code snippets? Share them through our pastebin.</p>
-    <div>
-        <x-label for="subject" value="{{ __('Subject') }}" />
-        <x-input id="subject" class="block mt-1 w-full" type="text" name="subject" autofocus />
-    </div>
-    <div>
-        <x-label for="subject" value="{{ __('Tags') }}" />
-        <x-input id="subject" class="block mt-1 w-full" type="text" name="subject" autofocus />
-    </div>
 
 
-
-
-
-
-
-        <livewire:markdown-editor />
-
-
-
-
-
-
+    <livewire:markdown-editor />
 
 </x-app-layout>
