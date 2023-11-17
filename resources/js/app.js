a486137f4c351d24cfa7bde07386214a9f8699a3
bakasibruceto <abla09027@gmail.com>
2023-11-17 11:00:15 +0800

    installed alpine.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import './bootstrap';
-
+import 'alpinejs';
 
 import hljs from 'highlight.js/lib/core';
 document.addEventListener('DOMContentLoaded', (event) => {
-    hljs.initHighlightingOnLoad();
+    hljs.highlightAll();
   });
 
 // import hljs from 'highlight.js';
@@ -13,6 +13,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
 //         hljs.highlightBlock(block);
 //     });
 // });
+
+
+    function copyCode(button) {
+        var codeElement = button.nextElementSibling.querySelector('code');
+        var textArea = document.createElement('textarea');
+        textArea.value = codeElement.innerText;
+        document.body.appendChild(textArea);
+        textArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textArea);
+        alert('Code copied to clipboard!');
+    }
+
+
 
 
 
