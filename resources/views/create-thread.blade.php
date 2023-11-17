68dbced2844088d0594502c04d9ba83d540db44a
bakasibruceto <abla09027@gmail.com>
2023-11-17 11:03:46 +0800

    temp

--- resources/views/create-thread.blade.php
+++ resources/views/create-thread.blade.php
@@ -1,11 +1,24 @@
 <x-app-layout>
-    <p>Create a new thread
-        Make sure you've read our rules before proceeding.
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-0">
+        <div class="flex flex-col flex-1">
+            <div class="md:grid md:grid-flow-row-dense md:grid-cols-3 md:grid-rows-1">
 
-        Please search for your question before posting your thread by using the search box in the navigation bar.
-        Want to share large code snippets? Share them through our pastebin.</p>
+                <div class="col-span-3">
 
+                    <livewire:markdown-editor />
 
-    <livewire:markdown-editor />
+                    {{-- <div class="hidden md:block lg:block w-3/7 md:h-60 pt-11 mr-6">
 
+                        <p class="bg-white shadow rounded-lg mb-6 p-4 w-full text-xl mx-0">
+                            Make sure you've read our rules before proceeding.
+
+                            Please search for your question before posting your thread by using the search box in the navigation bar.
+                            Want to share large code snippets? Share them through our pastebin.
+                        </p>
+
+                    </div> --}}
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
