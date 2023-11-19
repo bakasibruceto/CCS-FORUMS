a4848e0870a7c9d5b9a8b196a639196a250a241b
bakasibruceto <abla09027@gmail.com>
2023-11-19 23:14:32 +0800

    Create UserReply.php

---
+++ app/Models/UserReply.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserReply extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_reply';
+    protected $fillable = [
+        'post_id',
+        'user_id',
+        'markdown',
+    ];
+
+    public static function getRepliesByPostId($postId)
+    {
+        return self::where('post_id', $postId)->get();
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+}
