43bbc918fea74c86b50c59fbd8fbfaeb996c4dfe
bakasibruceto <abla09027@gmail.com>
2023-11-21 13:58:49 +0800

    database migration

--- database/migrations/2023_11_13_105036_create_forum_likes_table.php
+++ database/migrations/2023_11_13_105036_create_forum_likes_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('user_likes', function (Blueprint $table) {
+        Schema::create('forum_likes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('likesbyUser_id')->references('id')->on('users');
-            $table->foreignId('likes_id')->references('id')->on('users');
+            $table->foreignId('post_id')->references('id')->on('forum_posts');
+            $table->foreignId('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
