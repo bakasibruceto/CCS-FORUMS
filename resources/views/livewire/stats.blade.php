827505454035a58f04fe1c8227f31853623b5c8d
bakasibruceto <abla09027@gmail.com>
2023-11-23 02:45:28 +0800

    added backend stats

---
+++ resources/views/livewire/stats.blade.php
@@ -0,0 +1,57 @@
+<div wire:poll>
+    <div class="flex flex-wrap">
+        <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
+            <a href="{{route('admin.userTable')}}">
+                <div class="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
+                    <div class="p-3 text-white bg-indigo-600 bg-opacity-75 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                            stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+                        </svg>
+                    </div>
+
+                    <div class="mx-5">
+                        <h4 class="text-2xl font-semibold text-gray-700">{{ $userCount }}</h4>
+                        <div class="text-gray-500">Users</div>
+                    </div>
+                </div>
+            </a>
+
+        </div>
+
+        <div class="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 sm:mt-0">
+            <div class="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
+                <div class="p-3 text-white bg-orange-600 bg-opacity-75 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+                    </svg>
+                </div>
+
+                <div class="mx-5">
+                    <h4 class="text-2xl font-semibold text-gray-700">{{ $threadCount }}</h4>
+                    <div class="text-gray-500">Threads</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-full px-6 mt-6 sm:w-1/2 xl:w-1/3 xl:mt-0">
+            <div class="flex items-center px-5 py-6 bg-white rounded-md shadow-sm">
+                <div class="p-3 text-white bg-green-600 bg-opacity-75 rounded-full">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-6 h-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M9 12.75L11.25 15 15 9.75m-3-7.036A11.959 11.959 0 013.598 6 11.99 11.99 0 003 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285z" />
+                    </svg>
+                </div>
+
+                <div class="mx-5">
+                    <h4 class="text-2xl font-semibold text-gray-700">0</h4>
+                    <div class="text-gray-500">Online users</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
