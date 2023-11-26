bce5aa37a105adb9ff31710efa2bfd92a18582d1
bakasibruceto <abla09027@gmail.com>
2023-11-27 00:18:49 +0800

    filter tags dropdown

--- resources/views/user-dashboard.blade.php
+++ resources/views/user-dashboard.blade.php
@@ -37,10 +37,19 @@
                                 </span>
                                 <div
                                     class="inline-flex -space-x-px overflow-hidden rounded-md border bg-sky-900 shadow-sm">
-                                    <button
-                                        class="inline-block px-4 py-2 text-sm font-medium text-white hover:bg-sky-950 focus:relative">
-                                        Tag Filter
-                                    </button>
+                                    <div x-data="{ open: false }">
+                                        <button @click="open = !open" class="inline-block px-4 py-2 text-sm font-medium text-white hover:bg-sky-950 focus:relative">
+                                            Tag Filter
+                                        </button>
+
+                                        <div x-show="open" @click.away="open = false" class="absolute ml-[-100px] mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                                            <div class="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+                                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Option 1</a>
+                                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Option 2</a>
+                                                <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900" role="menuitem">Option 3</a>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -57,9 +66,11 @@
                 </div>
                 <x-left-box />
             </div>
+
             <x-footer class />
         </div>
     </div>
+
 </x-app-layout>
 
 {{-- <p>{{ \Illuminate\Support\Str::limit($post->markdown, 50) }}</p>
