40e03c6a617ebabb5faddae07de71eef8116e945
bakasibruceto <abla09027@gmail.com>
2023-11-27 00:19:27 +0800

    misc

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,32 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        // Schema::create('forum_posts', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->foreignId('user_id')->references('id')->on('users');
-        //     $table->string('title');
-        //     $table->string('tags')->nullable(true);
-        //     $table->longText('markdown');
-        //     $table->timestamps();
-        // });
-        // Schema::create('user_reply', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->foreignId('post_id')->references('id')->on('forum_posts');
-        //     $table->foreignId('user_id')->references('id')->on('users');
-        //     $table->longText('markdown');
-        //     $table->timestamps();
-        // });
-        // Schema::create('logs', function (Blueprint $table) {
-        //    $table->id();
-        //    $table->foreignId('user_id')->references('id')->on('users');
-        //    $table->string('actions');
-        //    $table->timestamps();
-        //});
+        //
     }
 }
