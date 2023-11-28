c8ede89b9fd93215ffdf62f54304bd5e2d7b2c6d
bakasibruceto <abla09027@gmail.com>
2023-11-28 20:25:38 +0800

    removed unnecessary files

--- resources/views/livewire/comment-component.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="flex">
-    <ion-icon wire:ignore name="chatbubble-ellipses-outline" class="text-2xl p-1"></ion-icon>
-    <p wire:poll.keep-alive.5000ms class="text-md pt-1">{{ $totalReplies }}</p>
-</div>
--- resources/views/livewire/follower-counter.blade.php
+++
@@ -1,6 +0,0 @@
-<div>
-    <div class="mr-4 p-3 text-center hover:bg-sky-100 rounded-lg">
-        <span class="text-sm block tracking-wide text-black">{{ $totalFollowers }}</span>
-        <span class="text-gray-600">followers</span>
-    </div>
-</div>
