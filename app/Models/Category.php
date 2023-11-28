547681559f13293d1c18f964c0e90f0d1f93ad0e
bakasibruceto <abla09027@gmail.com>
2023-11-29 03:04:17 +0800

    Create Category.php

---
+++ app/Models/Category.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'category';
+
+    protected $fillable = ['post_id', 'tag_id'];
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function tag()
+    {
+        return $this->belongsTo(Tags::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(ForumPost::class, 'post_id');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(ForumPost::class);
+    }
+
+}
