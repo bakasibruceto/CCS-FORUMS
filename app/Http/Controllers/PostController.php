5b91a8386e7bc7a65a76983914786d2d3aefadee
bakasibruceto <abla09027@gmail.com>
2023-11-30 03:13:45 +0800

    added edit function

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\ForumPost;
+use App\Models\Tags;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -28,6 +30,25 @@ class PostController extends Controller
         }
 
         return view('user-post', ['post' => $post]);
+    }
+
+    public function edit($id)
+    {
+        $post = ForumPost::find($id);
+
+        if (!$post) {
+            // Handle the case when the post with the given ID is not found
+            abort(404);
+        }
+
+        $categories = $post->categories; // Fetch the categories associated with the post
+        $tagIds = $categories->pluck('tag_id'); // Fetch the tag_ids associated with these categories
+
+        $getTag = Tags::whereIn('id', $tagIds)->get();
+
+        $tags = $getTag->pluck('name');// Fetch the tags that have an ID in the tagIds array
+
+        return view('edit-user-post', ['post' => $post, 'tags' => $tags]);
     }
 
 }
