203a701e9d7bb6670d6c7d90d18f9e53ceb011ea
bakasibruceto <abla09027@gmail.com>
2023-11-30 23:25:32 +0800

    add comments

--- app/Livewire/MarkdownParser.php
+++ app/Livewire/MarkdownParser.php
@@ -1,23 +1,31 @@
 <?php
 
 namespace App\Livewire;
+
 use App\Livewire\Traits\HighlighterJS;
 use App\Livewire\Traits\MarkdownEditor;
 use Livewire\Component;
 
 class MarkdownParser extends Component
 {
+    // Use the HighlighterJS and MarkdownEditor traits.
     use HighlighterJS, MarkdownEditor;
+
+    // This property will hold the parsed Markdown.
     public $parsedMarkdown;
 
+    // Listen for the 'markdownUpdated' event and call the 'updateMarkdown' method when it's fired.
     protected $listeners = ['markdownUpdated' => 'updateMarkdown'];
 
+    // It sets the initial Markdown and parses it.
     public function mount($markdown)
     {
         $this->markdown = $markdown;
         $this->parseMarkdown();
     }
 
+    // The 'updateMarkdown' method is called when the 'markdownUpdated' event is fired.
+    // It updates the Markdown and parses it.
     public function updateMarkdown($markdown)
     {
         $this->markdown = $markdown;
@@ -28,5 +36,4 @@ class MarkdownParser extends Component
     {
         return view('livewire.markdown-parser');
     }
-
 }
