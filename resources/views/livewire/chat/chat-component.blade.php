7543b726ab9256d05731d464f44e6e736ad09730
bakasibruceto <abla09027@gmail.com>
2023-12-02 09:52:54 +0800

    chat head

--- resources/views/livewire/chat/chat-component.blade.php
+++ resources/views/livewire/chat/chat-component.blade.php
@@ -6,13 +6,22 @@
             Livewire.on('messageReceived', () => { setTimeout(() => { $refs.chatMessages.scrollTop = $refs.chatMessages.scrollHeight }, 100) });
          ">
         <div id="chat-messages" x-ref="chatMessages" class="overflow-y-auto h-64 mb-4">
-            @foreach($messages as $message)
-                <div class="{{ $message->sender_id == auth()->id() ? 'text-right' : '' }}">
-                    <div class="{{ $message->sender_id == auth()->id() ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} inline-block rounded-lg px-4 py-2 mb-4">
-                        {{ $message->message }}
-                    </div>
+            @foreach ($messages as $message)
+            <div class="{{ $message->sender_id == auth()->id() ? 'flex justify-end' : 'flex' }}">
+                @if ($message->sender_id != auth()->id())
+                    <img src="{{ $message->sender->profile_photo_url }}" alt="Profile Picture"
+                        class="w-10 h-10 rounded-full mr-2">
+                @endif
+                <div
+                    class="{{ $message->sender_id == auth()->id() ? 'bg-blue-500 text-white' : 'bg-gray-300 text-black' }} inline-block rounded-lg px-4 py-2 mb-4">
+                    {{ $message->message }}
                 </div>
-            @endforeach
+                @if ($message->sender_id == auth()->id())
+                    <img src="{{ $message->sender->profile_photo_url }}" alt="Profile Picture"
+                        class="w-10 h-10 rounded-full ml-2">
+                @endif
+            </div>
+        @endforeach
         </div>
 
         <form wire:submit.prevent="sendMessage" class="flex" x-on:submit="setTimeout(() => { $refs.chatMessages.scrollTop = $refs.chatMessages.scrollHeight }, 100)">
