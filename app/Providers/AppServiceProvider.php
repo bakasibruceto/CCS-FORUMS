94166058ea3a512d0d48ca2661f92c66dd632f13
bakasibruceto <abla09027@gmail.com>
2023-12-12 17:03:07 +0800

    vite

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::useTailwind();
+
+        Blade::directive('vite', function ($expression) {
+            return "<?php echo vite({$expression}, true) ?>";
+        });
 
     }
 }
