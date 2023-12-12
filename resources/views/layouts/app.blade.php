7ea39c4e62539bfaf28c368050bcb8ae77a259a5
bakasibruceto <abla09027@gmail.com>
2023-12-13 00:47:21 +0800

    upp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,10 +14,8 @@
 
     <!-- Scripts -->
 
-    {{-- @vite(['resources/css/app.css', 'resources/css/a11y-dark.css', 'resources/js/app.js']) --}}
-    {{-- <link href="{{ mix('resources/css/app.css') }}" rel="stylesheet">
-    <link href="{{ mix('resources/css/a11y-dark.css') }}" rel="stylesheet">
-    <script src="{{ mix('resources/js/app.js') }}"></script> --}}
+    @vite(['resources/css/app.css', 'resources/css/a11y-dark.css', 'resources/js/app.js'])
+
     <!-- Styles -->
 
     {{-- <script src="{{ asset('path/to/highlight.js') }}"></script> --}}
