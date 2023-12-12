200db6bec0f8cdbf1e4cc4af16d5a558b0d57e63
bakasibruceto <abla09027@gmail.com>
2023-12-12 18:23:58 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel, { refreshPaths } from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        https: true,
+    },
     plugins: [
         laravel({
             input: [
